a6ee5e35ff8b2dbef67642fe75e467bb3f2d8b89
O-sulloc <bujjaf@gmail.com>
2022-01-06 12:14:39 +0900

    student blah blah

--- src/com/jh/s1/student/StudentMain.java
+++ src/com/jh/s1/student/StudentMain.java
@@ -1,23 +1,9 @@
-package com.jh.s1.student;
+//package com.jh.s1.student;
+//
+//public class StudentMain {
+//
+//	public static void main(String[] args) {
 
-public class StudentMain {
-
-	public static void main(String[] args) {
-		// make student 메서드를 여기서 호출해
-		// 이름,국영수,번호 출력해
-		// 입력은 make에서 받게 해
-
-		StudentUtil su = new StudentUtil();
-
-//		Student student = su.makeStudent();
-		Student [] s = StudentUtil.makeStudents();
-		System.out.println("이름: " + student.name);
-		System.out.println("번호: " + student.number);
-		System.out.println("국어: " + student.kor);
-		System.out.println("영어: " + student.eng);
-		System.out.println("수학: " + student.math);
-		System.out.println("총점: " + student.total);
-		System.out.println("평균: " + student.avg);
-	}
-
-}
+////	}
+//
+//}
